6f1813125a9d7073d950470dfe659f5700bb6624
HamzaMasood1 <hamzamasood183@gmail.com>
2023-09-20 01:28:23 +0100

    created the manager

--- basic-controller/main.go
+++ basic-controller/main.go
@@ -5,15 +5,28 @@ import (
 	mygroupv1alpha1 "github.com/HamzaMasood1/example-controllers/basic-controller/github.com/myid/myresource-crd/pkg/apis/mygroup.example.com/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
 func main() {
 	fmt.Println("hello")
 	scheme := runtime.NewScheme()
-	err := clientgoscheme.AddToScheme(scheme)
-	if err != nil {
-		fmt.Println(err)
-	}
 
-	mygroupv1alpha1.AddToScheme()
+	clientgoscheme.AddToScheme(scheme)
+	mygroupv1alpha1.AddToScheme(scheme)
+
+	mgr, err := manager.New(
+		config.GetConfigOrDie(),
+		manager.Options{
+			Scheme: scheme,
+		})
+	panicIf(err)
+
+}
+
+func panicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
