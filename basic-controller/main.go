9da963ad448efd6a833bd1262ccf9f19603cfdfa
HamzaMasood1 <hamzamasood183@gmail.com>
2023-09-20 12:17:34 +0100

    basic controller finished

--- basic-controller/main.go
+++ basic-controller/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	mygroupv1alpha1 "github.com/HamzaMasood1/example-controllers/basic-controller/github.com/myid/myresource-crd/pkg/apis/mygroup.example.com/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	controller "sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 func main() {
@@ -23,6 +29,45 @@ func main() {
 		})
 	panicIf(err)
 
+	controller, err := controller.New(
+		"my-operator",
+		mgr,
+		controller.Options{
+			Reconciler: &MyReconciler{},
+		})
+	panicIf(err)
+
+	err = controller.Watch(
+		&source.Kind{
+			Type: &mygroupv1alpha1.MyResource{},
+		},
+		&handler.EnqueueRequestForObject{},
+	)
+
+	panicIf(err)
+
+	err = controller.Watch(
+		&source.Kind{
+			Type: &corev1.Pod{},
+		},
+		&handler.EnqueueRequestForOwner{
+			OwnerType:    &corev1.Pod{},
+			IsController: true,
+		},
+	)
+	panicIf(err)
+
+	err = mgr.Start(context.Background())
+	panicIf(err)
+
+}
+
+type MyReconciler struct{}
+
+func (m *MyReconciler) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	//TODO implement me
+	fmt.Printf("reconcile %v\n", request)
+	return reconcile.Result{}, nil
 }
 
 func panicIf(err error) {
